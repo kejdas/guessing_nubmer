7eb4dd4d09ba7835e07065781024b96c0233224f
kejdas <64081579+kejdas@users.noreply.github.com>
2024-01-29 14:49:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import random
 
 def guess_number():
-    r_number = random.randint(0,100)
-    print("Hi, just try to guess number between 0 - 100")
+    # Generate a random number between 0 and 100
+    r_number = random.randint(0, 100)
+    print("Hi, just try to guess the number between 0 - 100")
 
+    # Initialize variables
     playing = True
     counter = 0
 
-    while playing == True:
+    # Main game loop
+    while playing:
+        # Get user input for their guess
         user_number = int(input())
 
+        # Check if the user's guess is correct
         if user_number == r_number:
             counter += 1
-            if counter <= 1:
-                print(f"Congratz, you win in {counter} move! That's incredible")
-            elif counter > 1:
-                print(f"Congratz, you win in {counter} moves!")
+            # Provide feedback on the number of moves taken to guess correctly
+            if counter == 1:
+                print(f"Congratulations! You win in {counter} move! That's incredible.")
+            else:
+                print(f"Congratulations! You win in {counter} moves.")
             playing = False
+        # Provide hints for incorrect guesses
         elif user_number < r_number:
-            print("sheesh bro, try a bigger number")
+            print("Sheesh bro, try a bigger number.")
             counter += 1
         elif user_number > r_number:
-            print("hold on man, you are going in wrong way! The number is too big")
+            print("Hold on, man! You are going in the wrong way. The number is too big.")
             counter += 1
 
+# Call the guess_number function to run the game
 guess_number()
